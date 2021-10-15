411cb164b66401d581001d9dd3f8db05a1f444e5
nguyen234-p <80658161+nguyen234-p@users.noreply.github.com>
2021-10-15 16:51:17 +0700

    câu 2

---
+++ app/src/main/java/com/example/a11_18084331_lethinguyen_dhktpm14ctt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a11_18084331_lethinguyen_dhktpm14ctt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
