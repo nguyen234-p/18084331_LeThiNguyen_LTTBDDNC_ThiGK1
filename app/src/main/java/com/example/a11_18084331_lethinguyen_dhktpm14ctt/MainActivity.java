f9d3d478b8466d056dd9d1c0d64dcc7d2c39e946
nguyen234-p <80658161+nguyen234-p@users.noreply.github.com>
2021-10-15 16:57:28 +0700

    câu 3

--- app/src/main/java/com/example/a11_18084331_lethinguyen_dhktpm14ctt/MainActivity.java
+++ app/src/main/java/com/example/a11_18084331_lethinguyen_dhktpm14ctt/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.a11_18084331_lethinguyen_dhktpm14ctt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img1, img2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img1 = findViewById(R.id.imageView);
+        img2 = findViewById(R.id.imageView2);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        img2.startAnimation(animation);
+        img1.startAnimation(animation);
     }
 }
